7114c7e2a32b1a3f48b2c1c06d57b3d72a2f4fe5
STEVEN TRAN <StevenTran@STEVENs-MacBook-Pro-2.local>
2017-12-20 14:20:36 -0800

    final upload

---
+++ game.js
@@ -0,0 +1,2 @@
+// Hangman Word Bank
+exports.wordBank = ['Pizza','Hamburger','Burrito','Taco','Sandwich'];
---
+++ index.js
@@ -0,0 +1,72 @@
+var prompt = require('prompt');
+var Word = require('./word.js');
+var Game = require('./game.js');
+
+prompt.start();
+
+game = {
+	wordBank : Game.wordBank,
+	wordsWon : 0,
+	guessesRemaining : 10,
+	guessedLetters: [],
+	currentWrd : null,
+	startGame : function(wrd) {
+		this.resetGuessesRemaining();
+		this.currentWrd = new Word.Word(this.wordBank[Math.floor(Math.random()* this.wordBank.length)]);
+		this.currentWrd.getLets();
+		console.log("Welcome to Food Hangman!\nGuess what food it is!");
+		console.log(this.currentWrd.wordRender() + '\n');
+		this.keepPromptingUser();
+	},
+	resetGuessesRemaining : function(){
+		this.guessRemaining = 10;
+	},
+	keepPromptingUser : function(){
+		var self = this;
+		prompt.get(['guessLetter'], function(err, result) {
+				console.log("");
+		    console.log('The letter you guessed is: ' + result.guessLetter);
+
+		    var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
+
+		    if (findHowManyOfUserGuess === 0) {
+					if (self.guessedLetters.indexOf(result.guessLetter) < 0) {
+            	self.guessedLetters.push(result.guessLetter);
+            	self.guessesRemaining--;
+            	console.log("You guessed a wrong letter!");
+          } else {
+            	console.log("You've already guessed this letter!");
+          }
+		    } else {
+			    	if (self.guessedLetters.indexOf(result.guessLetter) < 0) {
+	    				self.guessedLetters.push(result.guessLetter);
+				    	console.log('You guessed right!');
+				    } else {
+				    	console.log('Already guessed that letter');
+				    }
+		    		if (self.currentWrd.didWeFindTheWord()) {
+				    	console.log('You Won! The food was ' + self.currentWrd.word);
+							console.log('YAY!');
+				    	return;
+				    }
+			    }
+
+		    console.log('Guesses remaining: ', self.guessesRemaining);
+		    console.log(self.currentWrd.wordRender());
+				console.log("");
+		    console.log('Letters already guessed: ' + self.guessedLetters);
+
+
+		    if ((self.guessesRemaining > 0) && (self.currentWrd.found === false)){
+		    	self.keepPromptingUser();
+		    } else if(self.guessesRemaining === 0){
+			    	console.log('You lost! The food was', self.currentWrd.word);
+			    	console.log('Gotta catch um all!');
+			  } else{
+			    	console.log(self.currentWrd.wordRender());
+			  }
+		});
+	}
+};
+
+game.startGame();
